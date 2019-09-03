3b1a87150f8ecd1c63378130e2285cf0038e43ea
Will Alvarez <will.alvarez@gmail.com>
2019-09-03 17:20:00 -0500

    toggle y good bad material

---
+++ Assets/Scripts/toggleModels.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleModels : MonoBehaviour
+{
+    public GameObject Chassis;
+    public GameObject Welding;
+    // Start is called before the first frame update
+    public void toggleChassis(bool toggleTarget) {
+        Chassis.SetActive(toggleTarget);
+    }
+    public void toggleWelding(bool toggleTarget)
+    {
+        Welding.SetActive(toggleTarget);
+    }
+
+}
---
+++ Assets/Scripts/weldingValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weldingValidation : MonoBehaviour
+{
+    public GameObject Weld;
+    public Material Good;
+    public Material Bad;
+    // Start is called before the first frame update
+    public void goodWeld()
+    {
+        Weld.GetComponent<Renderer>().material = Good;
+    }
+    public void badWeld()
+    {
+        Weld.GetComponent<Renderer>().material = Bad;
+    }
+}
