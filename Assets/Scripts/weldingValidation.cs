649e961e9ecee525a41be8e2be0aa68e30e79b56
lepoups <robertodavila.c@hotmail.com>
2019-09-04 16:54:05 -0500

    Interface and Reset added

--- Assets/Scripts/weldingValidation.cs
+++ Assets/Scripts/weldingValidation.cs
@@ -1,19 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class weldingValidation : MonoBehaviour
 {
-    public GameObject Weld;
+    public GameObject Welds;
+    public GameObject Interface;
+    public GameObject PopUp;
     public Material Good;
     public Material Bad;
+    public Material Normal;
+    public Text ChildLenght;
+    public class Global
+    {
+        public static int CurrentChild = 0;
+    }
+
+
+
+
     // Start is called before the first frame update
-    public void goodWeld()
+    void Start()
     {
-        Weld.GetComponent<Renderer>().material = Good;
+        ChildLenght.text = Welds.transform.childCount.ToString();
+        Welds.transform.GetChild(Global.CurrentChild).gameObject.SetActive(true);
+
     }
-    public void badWeld()
+    public void GoodWeld()
     {
-        Weld.GetComponent<Renderer>().material = Bad;
+        var maxChilds = Welds.transform.childCount - 1;
+        if (Global.CurrentChild < maxChilds)
+        {
+            Welds.transform.GetChild(Global.CurrentChild).GetComponent<Renderer>().material = Good;
+            Global.CurrentChild = Global.CurrentChild + 1;
+            Welds.transform.GetChild(Global.CurrentChild).gameObject.SetActive(true);
+        }
+
+        else
+        {
+            Welds.transform.GetChild(Global.CurrentChild).GetComponent<Renderer>().material = Good;
+           
+            Interface.SetActive(false);
+            PopUp.SetActive(true);
+
+        }
+        
     }
+    public void BadWeld()
+    {
+        var maxChilds = Welds.transform.childCount - 1;
+        if (Global.CurrentChild < maxChilds)
+        {
+            Welds.transform.GetChild(Global.CurrentChild).GetComponent<Renderer>().material = Bad;
+            Global.CurrentChild = Global.CurrentChild + 1;
+            Welds.transform.GetChild(Global.CurrentChild).gameObject.SetActive(true);
+        }
+        else
+        {
+            Welds.transform.GetChild(Global.CurrentChild).GetComponent<Renderer>().material = Bad;
+            Interface.SetActive(false);
+            PopUp.SetActive(true);
+
+        }
+    }
+
+    public void Undo()
+    {
+        
+        if (Global.CurrentChild > 0)
+        {
+            Welds.transform.GetChild(Global.CurrentChild).GetComponent<Renderer>().material = Normal;
+            Welds.transform.GetChild(Global.CurrentChild).gameObject.SetActive(false);
+            Global.CurrentChild = Global.CurrentChild - 1;
+            Welds.transform.GetChild(Global.CurrentChild).GetComponent<Renderer>().material = Normal;
+
+        }
+
+    }
+
+    public void Resetinterface()
+    {
+        
+        foreach (Transform weld in Welds.transform)
+        {
+            weld.gameObject.SetActive(false);
+            weld.GetComponent<Renderer>().material = Normal;
+        }
+        Welds.transform.GetChild(0).gameObject.SetActive(true);
+        Global.CurrentChild = 0;
+        Interface.SetActive(true);
+        PopUp.SetActive(false);
+
+
+
+    }
+
+    public void Send()
+    {
+
+
+    }
+
+
+    
 }
+
+
+
